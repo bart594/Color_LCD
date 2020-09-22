e5e6fa388a4f5af39c4484455ae4bf5cbac0eb55
Daniel Claes <dclaes@smart-robotics.nl>
2020-09-22 22:12:46 +0200

    feature: working version to build with docker

--- firmware/build_with_docker.sh
+++ firmware/build_with_docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 mkdir -p build
 
-DOCKER_BUILDKIT=0 docker build -f docker/Dockerfile .
+DOCKER_BUILDKIT=0 docker build -f docker/Dockerfile . -t sw102_build
+docker run --rm -v $PWD/build:/build sw102_build /bin/bash -c "cp /source/SW102/_build/*.hex /build; cp /source/SW102/_release/*.zip /build"
